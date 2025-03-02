433c246a8b735566c4789e383a4e6df1ec679dc7
joao1306 <joaovictor1306@gmail.com>
2025-03-01 21:56:34 -0300

    Playlists

--- src/app/components/main/selecao/selecao.component.ts
+++ src/app/components/main/selecao/selecao.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-selecao',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './selecao.component.html',
   styleUrl: './selecao.component.css'
 })
